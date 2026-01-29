be25b8fc079aa82d6dd50307470128adf5f3179d
KasperHeuer <kasper100@ziggo.nl>
2026-01-29 19:11:50 +0100

    fix check #9

--- app/Jobs/EmailContactJob.php
+++ app/Jobs/EmailContactJob.php
@@ -39,6 +39,7 @@ class EmailContactJob implements ShouldQueue
             . "Met vriendelijke groet,\n"
             . "Kasper Heuer";
             try {
+                dd($body, $this->email);
                 Mail::raw($body, function ($message) {
                     $message->to($this->email)
                         ->subject("Contact op gelegd met Kasper Heuer")
