97bb05831127e92596535c9fff92d8c2cd5d8b4e
KasperHeuer <kasper100@ziggo.nl>
2026-01-29 19:38:58 +0100

    math fix

--- resources/views/components/math-form.blade.php
+++ resources/views/components/math-form.blade.php
@@ -4,7 +4,7 @@
     @csrf
 
     <label for="number" class="block text-gray-700 font-semibold">Enter a number</label>
-    <input required type="number" name="number" id="number" min="1" placeholder="e.g., {{ rand(1, 100) }}"
+    <input required type="number" name="number" id="numbe\r" min="0" placeholder="e.g., {{ rand(1, 100) }}"
         class="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-400">
 
     <button type="submit" class="w-full bg-blue-500 text-white py-2 rounded-md hover:bg-blue-600 transition-colors">
