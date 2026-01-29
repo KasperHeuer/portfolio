eaa63bb36e8642fd7807836daae73d6d7bcce6ab
KasperHeuer <kasper100@ziggo.nl>
2026-01-29 16:20:25 +0100

    Capitol fix

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use App\Http\Controllers\AboutController;
-use App\Http\Controllers\collatzController;
+use App\Http\Controllers\CollatzController;
 use App\Http\Controllers\ContactController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\ExponentsController;
