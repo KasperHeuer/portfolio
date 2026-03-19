519aaea7c534ab034e02b4c2559326b342455b69
KasperHeuer <kasper100@ziggo.nl>
2026-03-19 09:35:56 +0100

    Kleine fix

--- resources/views/lottie/home.blade.php
+++ resources/views/lottie/home.blade.php
@@ -2,7 +2,7 @@
     <section class="mx-auto w-full max-w-5xl px-4 py-10">
         <div class="mx-auto w-full max-w-xl rounded-xl border border-gray-700 bg-gray-900 p-4 shadow-lg">
             <div class="flex justify-center">
-                <dotlottie-wc src="{{ asset('lottie/Confetti.json') }}" style="width: 300px; height: 300px" autoplay
+                <dotlottie-wc src="{{ secure_asset('lottie/Confetti.json') }}" style="width: 300px; height: 300px" autoplay
                     loop></dotlottie-wc>
             </div>
         </div>
